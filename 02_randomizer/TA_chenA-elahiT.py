c2f9d3e504d2fc8924916741b09f97f6de25313c
Amanda Chen <achen01@stuy.edu>
2019-09-11 21:00:37 -0400

    added comments

--- 02_randomizer/TA_chenA-elahiT.py
+++ 02_randomizer/TA_chenA-elahiT.py
@@ -1,25 +1,29 @@
 import random
 
-KREWES = {'x':['a','b','c','d'],
-    'y':['e','f','g','h'],
-    'z':['i','j','k','l']}
+#list of students
+KREWES = {'orpheus':['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany', 'Amanda', 'Junhee', 'Jackie', 'Tyler', 'Emory', 'Ivan', 'Elizabeth', 'Pratham', 'Shaw', 'Eric', 'Yaru', 'Kelvin', 'Hong Wei', 'Michael', 'Kiran', 'Amanda', 'Joseph', 'Tanzim', 'David', 'Yevgeniy'],
+    'rex':['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
+    'endymion':['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']}
 
+#randomizer with team as paramerter
 def randStudent(team):
-    students = KREWES[team]
-    name = students[random.randint(0,len(students)-1)]
-    return(name)
+    students = KREWES[team] #retreiving list based on parameter passed
+    name = students[random.randint(0,len(students)-1)] #choses random index and assigns value to name
+    return(name) #returns random name
 
-x = []
-y = []
-z = []
-i = 0
 
-while (i < 10):
-    x.append(randStudent('x'));
-    y.append(randStudent('y'));
-    z.append(randStudent('z'));
-    i+=1
+#testing
+#x = []
+#y = []
+#z = []
+#i = 0
 
-print(x)
-print(y)
-print(z)
+#while (i < 10):
+#    x.append(randStudent('orpheus'));
+#    y.append(randStudent('rex'));
+#    z.append(randStudent('endymion'));
+#    i+=1
+
+#print(x)
+#print(y)
+#print(z)

75846c83f5504d7f0fd2e2d3d3ced34ddbb13fdc
Amanda Chen <achen01@stuy.edu>
2019-09-15 22:36:45 -0400

    created 2d array with name and percentage

---
+++ 03_csv/teamName_chenA-dossE.py
@@ -0,0 +1,19 @@
+#Amber Chen
+#SoftDev1 pd1
+#K6 -- StI/O: Divine your Destiny!
+#2019-09-15
+
+#opening file
+f = open("occupations.csv","r")
+
+#storing file into large string
+fString = f.read()
+
+#spliting string into array with name and percentage as an element
+fList = fString.split('\n')
+
+#creating new array with each element as an array with name and percentage
+fNewList = []
+for s in fList:
+    fNewList.append(s.rsplit(',',1))
+print(fNewList)

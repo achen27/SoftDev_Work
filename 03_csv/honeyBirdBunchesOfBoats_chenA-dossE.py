95c91151acc97a30b1b7fd51afd9aeefd57eff7f
Amanda Chen <achen01@stuy.edu>
2019-09-16 09:50:23 -0400

    wrote random function

--- 03_csv/honeyBirdBunchesOfBoats_chenA-dossE.py
+++ 03_csv/honeyBirdBunchesOfBoats_chenA-dossE.py
@@ -3,6 +3,8 @@
 #K6 -- StI/O: Divine your Destiny!
 #2019-09-15
 
+import random
+
 #opening file
 f = open("occupations.csv","r")
 
@@ -20,6 +22,31 @@ for s in fList:
 #added key value pairs to dictionary
 occupations = {}
 for a in fNewList[1:-2]:
-    occupations[a[0]] = float(a[1])
+    occupations[float(a[1])] = a[0]
 
-print(occupations['Management'])
+#print(occupations['Management'])
+
+#random funtion
+def randomOcc():
+    r = random.randint(1, 998) / 10.0
+    #print("random num: " + str(r))
+    for o in fNewList[1:-2]:
+        if (r >= 0):
+            #print(o[1])
+            r -= float(o[1])
+            #print("new num: " + str(r))
+        else:
+            return o[0]
+
+#testing function
+test = 0
+
+fTest = []
+for s in fList:
+    fTest.append(s.rsplit(',',1))
+print(fTest)
+for s in fList[1:-2]:
+    s[1] = 0
+print(fTest)
+
+#while (test < 1000):

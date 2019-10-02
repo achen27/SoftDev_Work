6d07211cfa228e27d4fd2b01704cfad23e248894
Amanda Chen <achen01@stuy.edu>
2019-10-02 09:36:03 -0400

    added flask app template

--- flaskTemplate/app.py
+++ flaskTemplate/app.py
@@ -2,24 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-name = "Team Egg Fried Rice Period 1"
-roster = "Amanda Chen & Tiffany Cao"
-correctU = "Eggs"
-correctP = "friedrice"
+name =
+roster =
 
 @app.route("/")
-
 def root():
     print(app)
-    return render_template('login.html',
+    return render_template('root.html',
                             team = name,
                             rost = roster)
 
 @app.route("/auth")
 def authenticate():
-    if (request.args['username'] == correctU and request.args['password'] == correctP) :
-       return redirect ("/")
-    else: return redirect ("/error")
+    return render_template('out.html',
+                            team = name,
+                            rost = roster,
+                            arg_user = str(request.args["username"]),
+                            arg_method = str(request.method))
 
 
 @app.route("/error")

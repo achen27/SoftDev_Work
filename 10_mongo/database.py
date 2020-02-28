563206c446b846167842dad7e136e1bd05b39680
Amanda Chen <achen01@Stuy.edu>
2020-02-28 14:09:49 +0000

    added json data to database

--- 10_mongo/database.py
+++ 10_mongo/database.py
@@ -2,10 +2,15 @@ from pymongo import MongoClient
 from bson.json_util import loads
 import json
 
-c = MongoClient()
+c = MongoClient("localhost",27017)
 db = c.RAM
 meteors = db.meteors
 
 f = open("meteorites.json","r")
-rString = f.readlines()
+rString = f.read()
 t = loads(rString)
+# for i in t:
+# 	print(i)
+# 	print("\n")
+
+meteors.insert_many(t)

c906eb87d1fa97ad4c55b9901735604cd1d11c40
Amanda Chen <achen01@stuy.edu>
2020-02-28 09:12:51 -0500

    moved to one file

--- 10_mongo/mongogo.py
+++ 10_mongo/mongogo.py
@@ -1,2 +1,12 @@
 from pymongo import MongoClient
+from bson.json_util import loads
 from pprint import pprint
+
+c = MongoClient()
+db = c.RAM
+meteors = db.meteors
+
+if (meteors in db.list_collection_names()):
+    f = open("meteorites.json","r")
+    rString = f.readlines()
+    t = loads(rString)

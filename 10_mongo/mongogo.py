d0acbd333ca99d22ddbe3cdb2d69056cc839e5cd
Amanda Chen <achen01@stuy.edu>
2020-03-03 08:41:13 -0500

    cleaned json

--- 10_mongo/mongogo.py
+++ 10_mongo/mongogo.py
@@ -10,3 +10,10 @@ if (meteors in db.list_collection_names()):
     f = open("meteorites.json","r")
     rString = f.readlines()
     t = loads(rString)
+
+# Dataset: Earth Meteorite Landings
+# Collection of data of 1000 meteors that landed on Earth by NASA
+# Includes their name, class, date fell, and coordinates where it fell
+# Link: https://data.nasa.gov/resource/y77d-th95.json
+# We imported that dataset onto mongo by reading the json into a string and loading it into mongo.
+# Before loading we cleaned the dataset be removing the last two extra columns for some rows

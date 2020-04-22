c6072a430d4989998c76deb9aed36688843bb083
Amanda Chen <achen01@stuy.edu>
2020-04-21 22:23:28 -0400

    rendered and added transitions

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -14,7 +14,7 @@ def cleaner():
     with open('static/gdp.json', 'r') as file:
         data = json.loads(file.read())[1]
     for i in range(len(data)):
-        valList.append([data[i]['date'],float(data[i]['value'])])
+        valList.append([int(data[i]['date']),float(data[i]['value'])])
     # print(valList[::-1])
     return valList[::-1]
 
@@ -22,7 +22,8 @@ def cleaner():
 @app.route("/")
 def helloworld():
     clean = cleaner()
-    return render_template('index.html', clean = clean)
+    print(clean)
+    return render_template('index.html', gdp = clean)
 
 if __name__ == "__main__":
     app.debug = True

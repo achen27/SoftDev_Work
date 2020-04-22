fb1dc8fc905b844055dfaf18c3691e41c3703670
Amanda Chen <achen01@stuy.edu>
2020-04-21 20:24:47 -0400

    fixed parsing of json

---
+++ 18_d3/app.py
@@ -0,0 +1,29 @@
+# Amanda "Amber" Chen, Elizabeth Doss (Team The Riftbreakers)
+# SoftDev2 pd1
+# K18 -- Come Up For Air
+# 2020-04-20
+
+from flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+# parses json file
+def cleaner():
+    data = []
+    valList = []
+    with open('static/gdp.json', 'r') as file:
+        data = json.loads(file.read())[1]
+    for i in range(len(data)):
+        valList.append([data[i]['date'],float(data[i]['value'])])
+    # print(valList[::-1])
+    return valList[::-1]
+
+# flask
+@app.route("/")
+def helloworld():
+    clean = cleaner()
+    return render_template('index.html', clean = clean)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
